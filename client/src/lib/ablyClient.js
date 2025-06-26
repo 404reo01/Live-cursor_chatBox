01716b7507fc6a57ff1e8151f159d016e0ba3bc2
Reo <theminachi@gmail.com>
2025-06-26 23:10:33 +0200

    configuration du .env

--- client/src/lib/ablyClient.js
+++ client/src/lib/ablyClient.js
@@ -1,3 +1,3 @@
 import * as Ably from 'ably'
 
-export const ably = new Ably.Realtime('NdgRcg.eFjK7g:Nv8D7CsuOmVOdB6j9nuxQaPZgwzT741A7iQqXLONo-I')
+export const ably = new Ably.Realtime(import.meta.env.VITE_ABLY_KEY)
--- client/src/lib/supabaseClient.js
+++ client/src/lib/supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
+console.log("Supabase URL:", import.meta.env.VITE_SUPABASE_URL)
+
 export const supabase = createClient(
-  'https://zgmlaxypbaqjyxeppwxw.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnbWxheHlwYmFxanl4ZXBwd3h3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5MjUyMzQsImV4cCI6MjA2NjUwMTIzNH0.1UqGAtavkUuIhuKxw_vYmF6IjMI90Ts8VHb_FiG4ZnM'
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
 )
